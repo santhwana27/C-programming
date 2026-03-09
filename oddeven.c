1cd45e0ad0882dd9fc285ab6d8b36c8f323c58a5
Santhwana P <santhwanap279@gmail.com>
2026-03-09 17:17:43 +0000

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+
+    printf("Enter the number: ");
+    scanf("%d",&a);
+
+    if(a%2==0)
+    {
+        printf("Even number");
+    }
+    else
+    {
+        printf("Odd number");
+    }
+
+    return 0;
+}
